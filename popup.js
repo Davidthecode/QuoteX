3804adb5181a5dca9da8c819664198b858b10068
David Ajibola <ajiboladavid0963@gmail.com>
2023-07-25 15:25:49 +0100

    initialt commit

---
+++ popup.js
@@ -0,0 +1,19 @@
+const loading = document.getElementById('loading')
+const quoteElement = document.getElementById('quoteElement')
+const authorElement = document.getElementById('authorElement')
+
+const getQuotes = async () => {
+    loading.style.display = 'block'
+    try {
+        const res = await fetch('https://zenquotes.io/api/random')
+        const data = await res.json()
+        loading.style.display = 'none'
+        quoteElement.innerHTML = `${data[0].q}`
+        authorElement.innerHTML = `${data[0].a}`
+    } catch (error) {
+        quoteElement.innerHTML = `oops... no qotes to show`
+    }
+
+}
+
+getQuotes()
